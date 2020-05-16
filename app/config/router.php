f073a65cbf2daad00f8c30be254434d472f7b137
Bkhufa <khufa95@gmail.com>
2020-05-16 16:11:20 +0700

    styling login & register page

--- app/config/router.php
+++ app/config/router.php
@@ -4,7 +4,6 @@ $router = $di->getRouter();
 
 // Define your routes here
 
-<<<<<<< HEAD
 // Default route
 $router->add('/', ['controller' => 'index', 'action' => 'index']);
 
@@ -17,6 +16,3 @@ $router->add('/user/signup', ['controller' => 'user', 'action' => 'signup']);
 $router->add('/user/logout', ['controller' => 'user', 'action' => 'logout']);
 
 $router->handle($_SERVER['REQUEST_URI']);
-=======
-$router->handle($_SERVER['REQUEST_URI']);
->>>>>>> 2ff15f707e76dcf1ba576d28e0fce4344322ba84
